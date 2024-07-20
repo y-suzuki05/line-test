3e894a0bbca3087ea8452c9ec6778b7bb4f3358a
yuki <momo_968_weekend@yahoo.co.jp>
2024-07-20 16:13:14 +0900

    セットアップ

---
+++ src/pages/line/confirmation.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react";
+const Confirmation = () => {
+  return <Box>外部ブラウザだよ</Box>
+};
+
+export default Confirmation;
---
+++ src/pages/line/index.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+const Line = () => {
+  return <Box>LINE内部ブラウザだよ</Box>
+};
+
+export default Line;
