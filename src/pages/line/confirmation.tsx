ca7d0e613531b51c280f7f11c3d5afa7c1d35bc0
yuki <momo_968_weekend@yahoo.co.jp>
2024-07-20 20:30:26 +0900

    liff初期化処理

--- src/pages/line/confirmation.tsx
+++ src/pages/line/confirmation.tsx
@@ -1,6 +1,47 @@
-import { Box } from "@chakra-ui/react";
+import { useEffect, useCallback, useRef, useState } from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
+import { Liff } from "@line/liff";
+
 const Confirmation = () => {
-  return <Box>外部ブラウザだよ</Box>
+  const liffRef = useRef<Liff | null>(null);
+  const [accessToken, setAccessToken] = useState<string>("");
+  const liffId = process.env.LIFF_ID;
+
+  const initLiff = useCallback(async () => {
+    try {
+      const liffModule = await import("@line/liff");
+      const liff = liffModule.default;
+
+      if (liffId) {
+        await liff.init({ liffId });
+      }
+
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+
+      liffRef.current = liff;
+    } catch (error) {
+      console.error("LIFFの初期化に失敗", error);
+    }
+  }, [liffId]);
+
+  const getAccessToken = () => {
+    const accessToken = liffRef.current?.getAccessToken();
+    accessToken && setAccessToken(accessToken);
+  };
+
+  useEffect(() => {
+    initLiff();
+  }, [initLiff]);
+
+  return (
+    <Box>
+      <Text>LINE連携しますか？</Text>
+      <Button onClick={() => getAccessToken()}>許可する</Button>
+      <Box>アクセストークン：{accessToken}</Box>
+    </Box>
+  );
 };
 
 export default Confirmation;
--- src/pages/line/index.tsx
+++ src/pages/line/index.tsx
@@ -1,6 +1,33 @@
+import { useEffect, useCallback, useRef } from "react";
 import { Box } from "@chakra-ui/react";
 
 const Line = () => {
+  const liffId = process.env.LIFF_ID;
+
+  const initLiff = useCallback(async () => {
+    try {
+      const liffModule = await import("@line/liff");
+      const liff = liffModule.default;
+
+      if (liffId) {
+        await liff.init({ liffId });
+      }
+
+      if (liff.isInClient()) {
+        liff.openWindow({
+          url: "https://line-test-six.vercel.app/line/confirmation",
+          external: true,
+        });
+      }
+    } catch (error) {
+      console.error("LIFFの初期化に失敗", error);
+    }
+  }, [liffId]);
+
+  useEffect(() => {
+    initLiff();
+  }, [initLiff]);
+
   return <Box>LINE内部ブラウザだよ</Box>
 };
 
